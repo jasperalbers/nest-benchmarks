c3750acb8209020780411ed119f46146a20ab1dc
Stine Vennemo <stine.vennemo@gmail.com>
2019-04-05 11:56:02 +0200

    update old files

--- old_files/analysis/analyse.py
+++ old_files/analysis/analyse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Usage: analyse [options] <file>
+Usage: analyse [options] <file>...
 
 Options:
     -h --help              Display this text and exit.
@@ -34,25 +34,41 @@ def plot_columns(csv, columns, output):
     num_rows = 2
     layout = (num_rows, int(np.ceil(len(columns)/num_rows)))
 
-    csv[columns].plot(subplots=True, layout=layout,
+    ax = csv[columns].plot(subplots=True, layout=layout,
                       kind='bar', legend=False, colormap='viridis')
 
     os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     if args['--output']:
         plt.savefig('../figures/{}'.format(args['--output']))
-    plt.show()
+
+    return ax
+    #plt.show()
 
 
 def main(args):
-    csv = pd.read_csv(args['<file>'])
-    csv = csv.set_index('RULE')
-    log.debug(csv)
+    frames = []
+    for bench in args['<file>']:
+        csv = pd.read_csv(bench)
+        csv = csv.set_index('SCALE')
+        log.debug(csv)
 
-    columns = args['--cols'].split(',') if args['--cols'] else None
-    print_columns(csv, columns)
-    if args['--plot']:
-        plot_columns(csv, columns, args['--output'])
+        columns = args['--cols'].split(',') if args['--cols'] else None
+        print_columns(csv, columns)
+        if args['--plot']:
+            frames.append(csv)
+            csv = pd.concat(frames)
+            plot_columns(csv, columns, args['--output'])
+
+    plt.show()      
+    # csv = pd.read_csv(args['<file>'])
+    # csv = csv.set_index('SCALE')
+    # log.debug(csv)
+
+    # columns = args['--cols'].split(',') if args['--cols'] else None
+    # print_columns(csv, columns)
+    # if args['--plot']:
+    #     plot_columns(csv, columns, args['--output'])
 
 
 if __name__ == '__main__':
@@ -60,4 +76,5 @@ if __name__ == '__main__':
     if args['--verbose']:
         log.setLevel(logging.DEBUG)
     log.debug(args)
+    print(args['<file>'])
     main(args)
